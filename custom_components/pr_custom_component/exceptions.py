6cc9c62849a280cad07e7984eeacca1eb7200324
Alan D. Tse <alandtse@gmail.com>
2021-03-23 10:49:07 +0000

    fix: add check to avoid deleting custom_components If custom_components not identified, it's possible the custom_components directory could be deleted. Add addiontal checks to prevent this.

--- custom_components/pr_custom_component/exceptions.py
+++ custom_components/pr_custom_component/exceptions.py
@@ -13,10 +13,6 @@ https://github.com/alandtse/pr_custom_component
 class PRCustomComponentException(Exception):
     """Class of PR Custom Component exceptions."""
 
-    pass
-
 
 class RateLimitException(PRCustomComponentException):
     """Class of exceptions for hitting retry limits."""
-
-    pass
